7c8093061947d7a47b188c60887af69de3b52724
Wahdat Jan <Wahdat.jan@trialx.com>
2020-06-11 23:13:40 +0530

    remove unnecessary libraries

--- app/src/main/java/com/app/schoolproject/login/LoginActivity.java
+++ app/src/main/java/com/app/schoolproject/login/LoginActivity.java
@@ -16,23 +16,16 @@ import com.app.schoolproject.registration.SignUpActivity;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 
-
-import dmax.dialog.SpotsDialog;
-
 public class LoginActivity extends AppCompatActivity {
-    TextInputEditText mEmail,mPassword ;
-    Button mLoginBtn;
+
     TextView mSignUpLink;
-    ConstraintLayout rootLayout;
-    private String  email,password;
-    SpotsDialog waitingDialog ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initViews();
-         waitingDialog= (SpotsDialog) new SpotsDialog.Builder().setContext(this).setMessage("Logging In...").build();
-        loginBtnClick();
+
         goToSignUpActivity();
     }
 
@@ -46,44 +39,9 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    private void loginBtnClick() {
-        mLoginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getTextFromEditTexts();
 
-                if (TextUtils.isEmpty(email)){
-                    mEmail.setError("Email is required");
-                }
-                else    if (TextUtils.isEmpty(password)){
-                    mPassword.setError("Password is required");
-                }
-
-                else if (password.length()<6 ){
-                    mPassword.setError("Password should be atleast 6 characters");
-                }
-                else {
-                    doLogin();
-                }
-            }
-
-            private void doLogin() {
-                waitingDialog.show();
-
-            }
-        });
-    }
-
-    private void getTextFromEditTexts() {
-
-        email = mEmail.getText().toString();
-        password = mPassword.getText().toString();
-    }
     private void initViews() {
-        mLoginBtn = findViewById(R.id.login_button);
-        mEmail = findViewById(R.id.editTextEmail);
-        mPassword = findViewById(R.id.editTextPassword);
-        rootLayout = findViewById(R.id.rootLayout);
+
         mSignUpLink = findViewById(R.id.signUpLink);
     }
 }
